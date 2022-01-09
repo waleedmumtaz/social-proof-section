2247e2219d9eaf66511a1682cf76e4e8d913f68f
Waleed <waleedthedev@gmail.com>
2022-01-09 20:59:53 +0500

    Clean up and setup according to style guides

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,27 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            'dark-magenta': 'hsl(300, 43%, 22%)',
+            'soft-pink': 'hsl(333, 80%, 67%)',
+          },
+          neutral: {
+            'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+            'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+          },
+        },
+      },
+      fontFamily: {
+        spartan: ['Spartan', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
